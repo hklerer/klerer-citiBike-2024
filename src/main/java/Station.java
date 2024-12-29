3b3f7a70bbfc9026428028e41b2e90e266c95f3b
hannahklerer <hklerer@student.touro.edu>
2024-12-29 12:12:03 -0500

    checkstyles

--- src/main/java/Station.java
+++ src/main/java/Station.java
@@ -1,10 +1,23 @@
 public class Station {
+    //CHECKSTYLE:OFF
     public String station_id;
+    //CHECKSTYLE:ON
+
     public String name;
     public double lon;
     public double lat;
+
+    //CHECKSTYLE:OFF
     public int num_ebikes_available;
+    //CHECKSTYLE:ON
+
+    //CHECKSTYLE:OFF
     public int num_docks_available;
+    //CHECKSTYLE:ON
+
+    //CHECKSTYLE:OFF
     public int num_bikes_available;
+    //CHECKSTYLE:ON
+
 
 }
