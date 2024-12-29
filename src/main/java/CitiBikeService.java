684bb1fd73bfc315598dede699dee39dbab5b90c
hannahklerer <hklerer@student.touro.edu>
2024-12-29 12:08:34 -0500

    did api call

---
+++ src/main/java/CitiBikeService.java
@@ -0,0 +1,10 @@
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.GET;
+
+public interface CitiBikeService {
+    @GET("/gbfs/en/station_information.json")
+    Single<Stations> stationInfo();
+
+    @GET("/gbfs/en/station_status.json")
+    Single<Stations> stationStatus();
+}
---
+++ src/main/java/Station.java
@@ -0,0 +1,10 @@
+public class Station {
+    public String station_id;
+    public String name;
+    public double lon;
+    public double lat;
+    public int num_ebikes_available;
+    public int num_docks_available;
+    public int num_bikes_available;
+
+}
