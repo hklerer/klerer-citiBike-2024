791e794eef25a4706d1b8067cdbbab11a7d8acd4
hannahklerer <hklerer@student.touro.edu>
2024-12-30 13:52:37 -0500

    wrote station by id method

--- src/main/java/klerer/citiBike/FindStations.java
+++ src/main/java/klerer/citiBike/FindStations.java
@@ -3,15 +3,18 @@ package klerer.citiBike;
 import klerer.citiBike.json.Data;
 import klerer.citiBike.json.Station;
 
+import java.util.Objects;
+
 public class FindStations {
 
-    public Station stationStatus(String stationId) {
+    public Station stationById(Station[] stations, String stationId) {
         Station foundStation = null;
-        for (Station station: ) {
-
+        for (Station station: stations) {
+            if (Objects.equals(stationId, station.station_id)) {
+                foundStation = station;
+            }
         }
-
-        return ;
+        return foundStation;
 
     }
 
--- src/main/java/klerer/citiBike/json/Station.java
+++ src/main/java/klerer/citiBike/json/Station.java
@@ -21,24 +21,24 @@ public class Station {
     public int num_bikes_available;
     //CHECKSTYLE:ON
 
-    public Station(String stationId, String name, int lon, int lat) {
-        //CHECKSTYLE:OFF
-        this.station_id = stationId;
-        //CHECKSTYLE:ON
-        this.name = name;
-        this.lon = lon;
-        this.lat = lat;
-    }
-
-    public Station(String stationId, String name, int lon, int lat,
-                          int numBikesAvailable, int numEbikesAvailable, int numDocksAvailable) {
-        //CHECKSTYLE:OFF
-        this.station_id = stationId;
-        //CHECKSTYLE:ON
-        this.num_bikes_available = numBikesAvailable;
-        this.num_ebikes_available = numEbikesAvailable;
-        this.num_docks_available = numDocksAvailable;
-    }
+//    public Station(String stationId, String name, int lon, int lat) {
+//        //CHECKSTYLE:OFF
+//        this.station_id = stationId;
+//        //CHECKSTYLE:ON
+//        this.name = name;
+//        this.lon = lon;
+//        this.lat = lat;
+//    }
+//
+//    public Station(String stationId, String name, int lon, int lat,
+//                          int numBikesAvailable, int numEbikesAvailable, int numDocksAvailable) {
+//        //CHECKSTYLE:OFF
+//        this.station_id = stationId;
+//        //CHECKSTYLE:ON
+//        this.num_bikes_available = numBikesAvailable;
+//        this.num_ebikes_available = numEbikesAvailable;
+//        this.num_docks_available = numDocksAvailable;
+//    }
 
 
 
