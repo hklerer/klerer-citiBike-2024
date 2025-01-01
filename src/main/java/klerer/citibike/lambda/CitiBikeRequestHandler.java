0d75abd940270bbdd26d58c9b86422931b814c91
hannahklerer <hklerer@student.touro.edu>
2024-12-31 21:40:21 -0500

    worked on lambda

---
+++ src/main/java/klerer/citibike/lambda/CitiBikeRequestHandler.java
@@ -0,0 +1,34 @@
+package klerer.citibike.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.google.gson.Gson;
+import klerer.citibike.CitiBikeService;
+import klerer.citibike.CitiBikeServiceFactory;
+import klerer.citibike.FindStations;
+import klerer.citibike.json.Station;
+import klerer.citibike.json.Stations;
+
+public class CitiBikeRequestHandler implements RequestHandler<APIGatewayProxyRequestEvent, CitiBikeResponse> {
+
+    @Override
+    public CitiBikeResponse handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+        String body = event.getBody();
+        Gson gson = new Gson();
+        CitiBikeRequest request = gson.fromJson(body, CitiBikeRequest.class);
+
+        CitiBikeServiceFactory factory = new CitiBikeServiceFactory();
+        CitiBikeService service = factory.getService();
+
+        Stations stationInfo = service.stationInfo().blockingGet();
+        Stations stationStatus = service.stationStatus().blockingGet();
+        FindStations findStations = new FindStations(stationInfo, stationStatus);
+
+        Station start = findStations.closestStationWithBikes(request.from.lat, request.from.lon);
+        Station end = findStations.closestStationWithSlots(request.to.lat, request.to.lon);
+
+        return new CitiBikeResponse(request.from, request.to, start, end);
+    }
+
+}
